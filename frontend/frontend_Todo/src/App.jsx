1d80d207bebf34c4b3939bd56933e12733465edd
Avinashpalofficial <apal73451@gmail.com>
2025-07-30 09:58:50 +0530

    fetching the data from backend

---
+++ frontend/frontend_Todo/src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react"
+import { useState } from "react"
+import axios from  "axios"
+function  App(){
+        const [todo ,setTodo] = useState([])
+        useEffect(()=>{
+         
+          
+           axios.get('http://localhost:3000/random-todo')
+           .then(function(res){
+            console.log(res.data);
+            
+            setTodo(res.data)
+           })
+
+        },[])
+  return (
+    <>
+       {todo.map(function(item){
+             return   <Todo  title={item.title} description={item.description} />
+       })}
+    </>
+  )
+}
+
+function Todo({title,description}){
+        return(
+          <div>
+                 <h1>{title}</h1>
+                 <br />
+                 <h2>{description}</h2>
+          </div>
+        )
+}
+
+export default App
